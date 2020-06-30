62a485ec65b3620ab4240fda2209ddf472f1cc9b
mlinlin <38514867+mlinlin@users.noreply.github.com>
2020-06-29 21:51:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 window.addEventListener("keyup", drawfunction);
 function drawfunction(){
-const thesvg = document.getElementById("thesvg");
-const firstarea = document.getElementById("firstarea");
-const contents = firstarea.value;
-console.log(contents);
+  const thesvg = document.getElementById("thesvg");
+  const firstarea = document.getElementById("firstarea");
+  const contents = firstarea.value;
+  const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+  const contents2 = contents.split(" ");
+  thesvg.appendChild(polygon);
+  for (location of contents2) {
+    const point = svg.createSVGPoint();
+    point.x = location.split(",")[0];
+    point.y = location.split(",")[1];
+    polygon.points.appendItem(point);
+  }
+  polygon {
+    stroke: black;
+  }
 }
