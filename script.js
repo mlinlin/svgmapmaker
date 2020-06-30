e9942a183486d46ae22bde9a5ec936826d544047
mlinlin <38514867+mlinlin@users.noreply.github.com>
2020-06-29 22:15:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,8 @@ function drawfunction(){
   const thesvg = document.getElementById("thesvg");
   const firstarea = document.getElementById("firstarea");
   const contents = firstarea.value;
-  const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-  polygon.setAttribute("id", "polygon1");
-  polygon.setAttribute("stroke", "black");
   const contents2 = contents.split(" ");
-  for (location of contents2) {
-    const point = svg.createSVGPoint();
-    point.x = location.split(",")[0];
-    point.y = location.split(",")[1];
-    polygon.points.appendItem(point);
-  }
-  thesvg.appendChild(polygon);
+  console.log(contents2);
+  const element = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  svg.appendChild(element);
 }
