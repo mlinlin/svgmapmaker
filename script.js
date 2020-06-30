8411ebfe87c9e8db990db4ba021f700e3fd02eaa
mlinlin <38514867+mlinlin@users.noreply.github.com>
2020-06-29 22:06:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function drawfunction(){
   polygon.setAttribute("id", "polygon1");
   polygon.setAttribute("stroke", "black");
   const contents2 = contents.split(" ");
-  thesvg.appendChild(polygon);
   for (location of contents2) {
     const point = svg.createSVGPoint();
     point.x = location.split(",")[0];
     point.y = location.split(",")[1];
     polygon.points.appendItem(point);
   }
+  thesvg.appendChild(polygon);
 }
