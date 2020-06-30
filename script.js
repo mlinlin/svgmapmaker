102baaad6f02aaf0fed7e64791862495db1b887f
mlinlin <38514867+mlinlin@users.noreply.github.com>
2020-06-29 21:20:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+window.addEventListener("keyup", drawfunction);
+function drawfunction(){
 const thesvg = document.getElementById("thesvg");
+const firstarea = document.getElementById("firstarea");
+}
