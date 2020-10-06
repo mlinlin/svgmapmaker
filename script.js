bcb7524d42e26a673f91bc2f29f5a8a0f4ca57eb
mlinlin <38514867+mlinlin@users.noreply.github.com>
2020-10-06 18:48:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ function drawfunction(){
   const thesvg = document.getElementById("thesvg");
   const firstarea = document.getElementById("firstarea");
   const contents = firstarea.value;
-  const contents1 = contents.split("<coordinates>")[1];
-  const contents2 = contents1.split(",0 ");
-  console.log(contents2);
-  const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-  polygon.setAttribute("id", "polygon1");
-  polygon.setAttribute("stroke", "black");
-  for (const location of contents2) {
-    const point = thesvg.createSVGPoint();
-    point.x = location.split(",")[0]+180;
-    point.y = location.split(",")[1]+180;
-    polygon.points.appendItem(point);
+  if (contents.split("<coordinates>")[1] != null){
+    const contents1 = contents.split("<coordinates>")[1];
+    const contents2 = contents1.split(",0 ");
+    console.log(contents2);
+    const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    polygon.setAttribute("id", "polygon1");
+    polygon.setAttribute("stroke", "black");
+    for (const location of contents2) {
+      const point = thesvg.createSVGPoint();
+      point.x = location.split(",")[0]+180;
+      point.y = location.split(",")[1]+180;
+      polygon.points.appendItem(point);
+    }
+    thesvg.appendChild(polygon);
   }
-  thesvg.appendChild(polygon);
 }
