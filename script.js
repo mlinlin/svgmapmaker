d27352125b306f8ba70db876dc49ce65a0b26c69
mlinlin <38514867+mlinlin@users.noreply.github.com>
2020-10-06 18:35:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function drawfunction(){
   const thesvg = document.getElementById("thesvg");
   const firstarea = document.getElementById("firstarea");
   const contents = firstarea.value;
-  const contents1 = contents.split("<coordinates>")[1]
-  const contents2 = contents.split("0");
+  const contents1 = contents.split("<coordinates>")[1].split("</coordinates>")[0];
+  const contents2 = contents1.split("0");
   console.log(contents2);
   const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
   polygon.setAttribute("id", "polygon1");
